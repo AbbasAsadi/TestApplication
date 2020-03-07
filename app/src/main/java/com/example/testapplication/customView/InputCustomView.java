429379c3cb4e7cdcdd910eb72eceb2d94ca6756f
abbasasadi <abbas.asadi1996@gmail.com>
2020-03-07 11:41:08 +0330

    add keyboard type correct logic but still have work clean code

--- app/src/main/java/com/example/testapplication/customView/InputCustomView.java
+++ app/src/main/java/com/example/testapplication/customView/InputCustomView.java
@@ -2,6 +2,11 @@ package com.example.testapplication.customView;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Rect;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.SpannableStringBuilder;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 
@@ -9,47 +14,51 @@ import com.example.testapplication.R;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class InputCustomView extends TextInputEditText {
+    private final int REQUIRED = 0;
+    private final int UNREQUIRED = 1;
     private Context mContext;
-    //private TextInputEditText mEditText;
-
     private int mTextType;
-    //private String mText;
+    private String mText;
     private String mHint;
     private TypedArray mTypeArray;
-    // private boolean mIsRequired;
+    private int mIsRequired = UNREQUIRED;
 
     public InputCustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.mContext = context;
         mTypeArray = mContext.getTheme().obtainStyledAttributes(attrs, R.styleable.InputCustomView, 0, 0);
-       /* LayoutInflater.from(context).inflate(R.layout.custom_view_input, this, true);
-        mEditText = findViewById(R.id.custom_text_input);*/
         try {
-           // mText = mTypeArray.getString(R.styleable.InputCustomView_customViewText);
+            mText = mTypeArray.getString(R.styleable.InputCustomView_customViewText);
             mHint = mTypeArray.getString(R.styleable.InputCustomView_customViewHint);
-            // mIsRequired = mTypeArray.getBoolean(R.styleable.InputCustomView_customViewIsRequired, false);
+            mIsRequired = mTypeArray.getInt(R.styleable.InputCustomView_customViewIsRequired, UNREQUIRED);
             mTextType = mTypeArray.getInt(R.styleable.InputCustomView_customViewType, 0);
 
         } finally {
             mTypeArray.recycle();
         }
-        if (/*isMyTextValid() &&*/ isMyHintValid()) {
-            init(mHint);
-        } else {
-            setError("input data is wrong");
+        if (mHint != null) {
+            setHint(mHint);
         }
 
+        switch (mTextType) {
+            case 1:
+                setInputType(InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+                break;
+            case 2:
+                setInputType(InputType.TYPE_CLASS_PHONE);
+                break;
+            case 3:
+                setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                break;
+        }
     }
 
-    private boolean isMyHintValid() {
-        return mHint != null;
-    }
 
-    private boolean isMyTextValid(CharSequence text) {
-        String mText = text.toString();
-        if (mText != null) {
+    private boolean isMyTextValid(String text) {
+
+        if (text != null) {
             if (mTextType == TextType.EMAIL.getValue()) {
-                if (mText.isEmpty() || !mText.contains("@") || mText.lastIndexOf(".") < mText.lastIndexOf("@")) {
+                if (text.isEmpty() || !text.contains("@") || text.lastIndexOf(".") < text.lastIndexOf("@")) {
                     setError("ایمیل وارد شده صحیح نمیباشد!!!");
                     //throw new RuntimeException("no suitable email provided");
                     return false;
@@ -57,7 +66,7 @@ public class InputCustomView extends TextInputEditText {
                     return true;
             }
             if (mTextType == TextType.NAME.getValue()) {
-                if (mText.matches(".*\\d.*")) {
+                if (text.matches(".*\\d.*")) {
                     setError("نام نباید شامل عدد باشد!!!");
 //                    throw new RuntimeException("no suitable name provided");
                     return false;
@@ -65,7 +74,7 @@ public class InputCustomView extends TextInputEditText {
                     return true;
             }
             if (mTextType == TextType.PHONE_NUMBER.getValue()) {
-                if (mText.length() != 11 || !mText.startsWith("09")) {
+                if (text.length() != 11 || !text.startsWith("09")) {
                     setError("شماره وارد شده صحیح نمیباشد!!!");
 //                    throw new RuntimeException("no suitable phoneNumber provided");
                     return false;
@@ -73,32 +82,24 @@ public class InputCustomView extends TextInputEditText {
                     return true;
             }
         } else {
-            return false;
-           /* if (mIsRequired) {
-                mEditText.setError("فیلد خالی است!!!");
+            if (mIsRequired == REQUIRED) {
+                setError("فیلد خالی است!!!");
                 // throw new RuntimeException("no text provided");
                 return false;
             } else {
                 return true;
-            }*/
+            }
         }
         return false;
     }
 
 
     @Override
-    public void setText(CharSequence text, BufferType type) {
-        if (text!= null && !text.toString().isEmpty() && isMyTextValid(text)) {
-            super.setText(text, type);
-        }else {
-            setError("ورودی نامعتبر است");
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect);
+
+        if (!isMyTextValid(getText().toString())) {
+            focused = true;
         }
     }
-
-    private void init(String hint) {
-        setHint(hint);
-        //setText(text);
-    }
-
-
 }
