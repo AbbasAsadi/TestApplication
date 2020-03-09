700c38cb1018b3b0053c5716c9615824ecb3d563
abbasasadi <abbas.asadi1996@gmail.com>
2020-03-09 17:00:06 +0330

    improve validation fix bug optional fix bug should focus to check validate

--- app/src/main/java/com/example/testapplication/customView/InputCustomView.java
+++ app/src/main/java/com/example/testapplication/customView/InputCustomView.java
@@ -2,25 +2,29 @@ package com.example.testapplication.customView;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Rect;
 import android.text.InputFilter;
 import android.text.InputType;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.Patterns;
+import android.view.LayoutInflater;
 
 import com.example.testapplication.R;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.regex.Matcher;
 
-public class InputCustomView extends TextInputEditText {
+public class InputCustomView extends TextInputLayout {
+    private final String TAG = "InputCustomView";
     private final int REQUIRED = 0;
-    private final int UNREQUIRED = 1;
+    private final int OPTIONAL = 1;
 
     private final int NAME = 1;
     private final int PHONE_NUMBER = 2;
     private final int EMAIL = 3;
 
+    private TextInputEditText mEditText;
     private Context mContext;
     private int mTextType;
     private String mHint;
@@ -31,10 +35,13 @@ public class InputCustomView extends TextInputEditText {
     public InputCustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.mContext = context;
+        LayoutInflater.from(mContext).inflate(R.layout.custom_view_input, this, true);
+        mEditText = findViewById(R.id.custom_text_input);
+
         mTypeArray = mContext.getTheme().obtainStyledAttributes(attrs, R.styleable.InputCustomView, 0, 0);
         try {
             mHint = mTypeArray.getString(R.styleable.InputCustomView_customViewHint);
-            mIsRequired = mTypeArray.getInt(R.styleable.InputCustomView_customViewIsRequired, UNREQUIRED);
+            mIsRequired = mTypeArray.getInt(R.styleable.InputCustomView_customViewIsRequired, OPTIONAL);
             mTextType = mTypeArray.getInt(R.styleable.InputCustomView_customViewType, 0);
         } finally {
             mTypeArray.recycle();
@@ -42,112 +49,108 @@ public class InputCustomView extends TextInputEditText {
         if (mHint != null) {
             setHint(mHint);
         }
-
         switch (mTextType) {
             case NAME:
-                setInputType(InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+                mEditText.setInputType(InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
                 break;
             case PHONE_NUMBER:
-                setInputType(InputType.TYPE_CLASS_PHONE);
+                mEditText.setInputType(InputType.TYPE_CLASS_PHONE);
                 setMaxLengthForPhoneNumber();
                 break;
             case EMAIL:
-                setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                mEditText.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
                 break;
         }
+        mEditText.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus) {
+                isMyTextValid();
+                Log.d(TAG, "InputCustomView: " + mIsValid);
+            }
+        });
     }
 
     private void setMaxLengthForPhoneNumber() {
         int maxLength = 11;
         InputFilter[] fArray = new InputFilter[1];
         fArray[0] = new InputFilter.LengthFilter(maxLength);
-        setFilters(fArray);
+        mEditText.setFilters(fArray);
     }
 
     public boolean isValid() {
         return mIsValid;
     }
 
-    private void isMyTextValid(String text) {
-        if (text != null && !text.isEmpty()) {
-            switch (mTextType) {
-                case NAME:
-                    isNameValid(text);
-                    break;
-                case EMAIL:
-                    isEmailValid(text);
-                    break;
-                case PHONE_NUMBER:
-                    isPhoneNumberValid(text);
-                    break;
-                default:
-            }
+    public boolean isMyTextValid() {
+        String text = mEditText.getText().toString();
+        switch (mTextType) {
+            case NAME:
+                isNameValid(text);
+                break;
+            case EMAIL:
+                isEmailValid(text);
+                break;
+            case PHONE_NUMBER:
+                isPhoneNumberValid(text);
+                break;
+            default:
         }
+        return mIsValid;
     }
 
     private void isPhoneNumberValid(String text) {
-        if (text.length() == 11 && text.startsWith("09")) {
-            Matcher matcher = Patterns.PHONE.matcher(text);
-            mIsValid = matcher.matches();
-        } else {
-            mIsValid = false;
-        }
+        if (!text.isEmpty()) {
+            if (text.length() == 11 && text.startsWith("09")) {
+                Matcher matcher = Patterns.PHONE.matcher(text);
+                mIsValid = matcher.matches();
+            } else {
+                mIsValid = false;
+            }
 
-        if (!mIsValid) {
-            setError("شماره وارد شده صحیح نمیباشد!!!");
+            if (!mIsValid) {
+                setError("شماره وارد شده صحیح نمیباشد!!!");
+            }
+        } else if (mIsRequired == REQUIRED) {
+            Log.d(TAG, "isNameValid: in second if");
+            mIsValid = false;
+            setError("فیلد خالی");
+        } else if (mIsRequired == OPTIONAL) {
+            Log.d(TAG, "isNameValid: in third if");
+            mIsValid = true;
         }
     }
 
     private void isNameValid(String text) {
-        if (text.matches(".*\\d.*")) {
-            setError("نام نباید شامل عدد باشد!!!");
+        if (!text.isEmpty()) {
+            Log.d(TAG, "isNameValid: in first if");
+            if (text.matches(".*\\d.*")) {
+                setError("نام نباید شامل عدد باشد!!!");
+                mIsValid = false;
+            } else {
+                mIsValid = true;
+            }
+        } else if (mIsRequired == REQUIRED) {
+            Log.d(TAG, "isNameValid: in second if");
             mIsValid = false;
-        } else {
+            setError("فیلد خالی");
+        } else if (mIsRequired == OPTIONAL) {
+            Log.d(TAG, "isNameValid: in third if");
             mIsValid = true;
         }
     }
 
     private void isEmailValid(String text) {
-        Matcher matcher = Patterns.EMAIL_ADDRESS.matcher(text);
-        mIsValid = matcher.matches();
-        if (!mIsValid)
-            setError("ایمیل وارد شده صحیح نمیباشد!!!");
-
-       /* if (!text.contains("@") ||
-                text.lastIndexOf(".") < text.lastIndexOf("@") ||
-                text.lastIndexOf(".") == text.length() - 1) {
-            setError("ایمیل وارد شده صحیح نمیباشد!!!");
+        if (!text.isEmpty()) {
+            Matcher matcher = Patterns.EMAIL_ADDRESS.matcher(text);
+            mIsValid = matcher.matches();
+            if (!mIsValid)
+                setError("ایمیل وارد شده صحیح نمیباشد!!!");
+        } else if (mIsRequired == REQUIRED) {
+            Log.d(TAG, "isNameValid: in second if");
             mIsValid = false;
-        } else {
+            setError("فیلد خالی");
+        } else if (mIsRequired == OPTIONAL) {
+            Log.d(TAG, "isNameValid: in third if");
             mIsValid = true;
-        }*/
-    }
-
-
-    public boolean isInputEmpty() {
-        String text = getText().toString();
-        if (text.isEmpty()) {
-            if (mIsRequired == REQUIRED) {
-                setError("فیلد خالی است!!!");
-                return true;
-            } else {
-                return false;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
-        super.onFocusChanged(focused, direction, previouslyFocusedRect);
-
-        String text = getText().toString();
-        isMyTextValid(text);
-
-        if (mIsValid) {
-            super.onFocusChanged(false, direction, previouslyFocusedRect);
-        } else {
-            super.onFocusChanged(true, direction, previouslyFocusedRect);
         }
     }
 }
--- app/src/main/java/com/example/testapplication/view/FormActivity.java
+++ app/src/main/java/com/example/testapplication/view/FormActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 
 import com.example.testapplication.R;
 import com.example.testapplication.customView.InputCustomView;
@@ -17,6 +18,7 @@ public class FormActivity extends AppCompatActivity {
     private InputCustomView mCustomViewEmail;
     private InputCustomView mCustomViewPhoneNumber;
     private Button mConfirmButton;
+    private NestedScrollView mScrollView;
 
     public static Intent newIntent(Context context) {
         return new Intent(context, FormActivity.class);
@@ -30,18 +32,16 @@ public class FormActivity extends AppCompatActivity {
 
 
         mConfirmButton.setOnClickListener(v -> {
-            if (!mCustomViewEmail.isValid() && mCustomViewEmail.isInputEmpty()) {
-                mCustomViewEmail.setFocusable(true);
-                mCustomViewEmail.requestFocus();
-            } else if (!mCustomViewPhoneNumber.isValid() && mCustomViewPhoneNumber.isInputEmpty()) {
-                mCustomViewPhoneNumber.setFocusable(true);
-                mCustomViewPhoneNumber.requestFocus();
-            } else if (!mCustomViewName.isValid() && mCustomViewName.isInputEmpty()) {
-                mCustomViewName.setFocusable(true);
-                mCustomViewName.requestFocus();
-            } else if (!mCustomViewFamily.isValid() && mCustomViewFamily.isInputEmpty()) {
-                mCustomViewFamily.setFocusable(true);
-                mCustomViewFamily.requestFocus();
+            mConfirmButton.setFocusable(true);
+            mConfirmButton.requestFocus();
+            if (!mCustomViewEmail.isMyTextValid()) {
+                mScrollView.smoothScrollTo(0 , mCustomViewEmail.getTop() - 24 );
+            } else if (!mCustomViewPhoneNumber.isMyTextValid()) {
+                mScrollView.post(() -> mScrollView.smoothScrollTo(0 , mCustomViewPhoneNumber.getTop() - 24));
+            } else if (!mCustomViewName.isMyTextValid()) {
+                mScrollView.post(() -> mScrollView.smoothScrollTo(0 , mCustomViewName.getTop() - 24));
+            } else if (!mCustomViewFamily.isMyTextValid()) {
+                mScrollView.post(() -> mScrollView.smoothScrollTo(0 , mCustomViewFamily.getTop() - 24));
             } else {
                 Toast.makeText(this, "ثبت نام با موفقیت انجام شد.", Toast.LENGTH_SHORT).show();
                 startActivity(MainActivity.newIntent(this));
@@ -50,6 +50,7 @@ public class FormActivity extends AppCompatActivity {
     }
 
     private void initUI() {
+        mScrollView = findViewById(R.id.scroll_view);
         mConfirmButton = findViewById(R.id.confirm_button);
         mCustomViewName = findViewById(R.id.name_input);
         mCustomViewEmail = findViewById(R.id.email_input);
